81331ca512ec7fbb7bab2af6effc55c306a7caae
bherry <yann.herren@bbcag.ch>
2019-07-10 12:39:20 +0200

    Updates

---
+++ precache-manifest.bd907bab265a1b3c14016867536b3ac6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a9848e1b7db547ca402f71fb7433e27",
+    "url": "/TinyClipboard/index.html"
+  },
+  {
+    "revision": "58c35b8f8536bfa2b9b0",
+    "url": "/TinyClipboard/static/css/main.a69ee5dd.chunk.css"
+  },
+  {
+    "revision": "cc8c6d0241e893973456",
+    "url": "/TinyClipboard/static/js/2.422eae5a.chunk.js"
+  },
+  {
+    "revision": "58c35b8f8536bfa2b9b0",
+    "url": "/TinyClipboard/static/js/main.30a0e56e.chunk.js"
+  },
+  {
+    "revision": "cf817cf4bba41dea1046",
+    "url": "/TinyClipboard/static/js/runtime~main.b325145f.js"
+  }
+]);
